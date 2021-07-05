04d885bab7ef42826ea7fc61a0d7865e01613389
woba6255 <vova6255@gmail.com>
2021-07-05 16:18:51 +0300

    setupProxy.js test

--- packages/web-app/src/App.js
+++ packages/web-app/src/App.js
@@ -1,8 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 import { Button } from "@mono/ui-components";
+import { useEffect, useState } from "react";
+
+const params = {
+    username: '123',
+    password: '123'
+}
+const headers = {
+    Authorization: `Basic ${btoa(`${params.username}:${params.password}`)}`,
+    'Content-Type': 'application/json',
+};
 
 function App() {
+    const [buttonName, setButtonName] = useState()
+
+    const onClick = () => {
+        fetch('/dcpc/stations', {headers})
+            .then((r) => r.json())
+            .then((data) => {
+                setButtonName(JSON.stringify(data))
+            })
+    }
+
+
     return (
         <div className="App">
             <header className="App-header">
@@ -10,7 +31,7 @@ function App() {
                 <p>
                     Lets see you @mono/ui-components button:
                 </p>
-                <Button></Button>
+                <Button onClick={onClick}>{buttonName}</Button>
             </header>
         </div>
     );
